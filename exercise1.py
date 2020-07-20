4ee57f6eb9714d27e9b08795c3b58f6995608253
bec-dill <rebeccadillon1984@gmail.com>
2020-07-20 19:26:22 +1000

    Initial commit

---
+++ exercise1.py
@@ -0,0 +1,33 @@
+print("Q1")
+
+first_int = 3
+second_int = 9
+
+print(first_int + second_int)
+
+print(second_int - first_int)
+
+first_float = 3.0
+
+print(first_float - second_int)
+
+print("Q2")
+
+print(first_int * second_int)
+
+print(-first_int * second_int)
+
+print(first_float * -second_int)
+
+print("Q3")
+
+
+
+
+print("Q4")
+
+name = input("What is your name?")
+height = input(f"Hi {name}, how tall are you?")
+print(f"{name}, you are {height} cms tall!")
+
+
