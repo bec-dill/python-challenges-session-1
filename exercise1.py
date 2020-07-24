7e7803c56c9381dfbe578d2a2b1ed74ebab77bd8
bec-dill <rebeccadillon1984@gmail.com>
2020-07-24 20:18:38 +1000

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -21,13 +21,16 @@ print(first_float * -second_int)
 
 print("Q3")
 
-
+km = int(input("How many kilometers have you run?"))
+m = km * 1000
+cm = km * 10000
+print (f"Wow, you have run {m}m or {cm}cm!")
 
 
 print("Q4")
 
 name = input("What is your name?")
-height = input(f"Hi {name}, how tall are you?")
-print(f"{name}, you are {height} cms tall!")
+height = int(input(f"Hi {name}, how tall are you?"))
+print(f"{name} is {height} cms tall.")
 
 
